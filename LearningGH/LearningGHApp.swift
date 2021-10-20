12f2bdd7faa0d8d3a1722a8ba09127dcb757c9c7
Enrique Montero <enmontero0204@hotmail.com>
2021-10-19 20:33:34 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Enrique Montero on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
